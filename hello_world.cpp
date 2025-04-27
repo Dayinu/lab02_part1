2b5c3184f520b4e9dbe7b509e90e20ab7cdee45f
Dayinu <vlada.privet@gmail.com>
2025-04-27 14:57:11 -0400

    Добавлены поясняющие комментарии

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <string>
 
+// Главная функция программы
 int main() {
+    // Объявление переменной для хранения имени
     std::string name;
+    
+    // Запрос имени пользователя
     std::cout << "Enter your name: ";
     std::cin >> name;
+    
+    // Вывод приветствия
     std::cout << "Hello world from " << name << std::endl;
+    
+    // Возврат успешного статуса выполнения
     return 0;
 }
